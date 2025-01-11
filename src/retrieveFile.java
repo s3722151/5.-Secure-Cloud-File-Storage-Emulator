958d0c15118e3a90a3c8705186ea76400dc9db3b
Zetazone <37312796+Zetazone@users.noreply.github.com>
2025-01-11 11:00:39 +1100

    Added a line which specifies where the program should open.

--- src/retrieveFile.java
+++ src/retrieveFile.java
@@ -2,12 +2,17 @@ import java.awt.Desktop;
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.FlowLayout;
 
 public class retrieveFile {
 	
-	//Methods
+	//Method: https://stackoverflow.com/questions/40255039/how-to-choose-file-in-java
 	static void selectedFile() {
 	    JFileChooser chooser = new JFileChooser();
+	    
+	    chooser.setCurrentDirectory(new File("."));
+	    //How to open at select place: chooser.setCurrentDirectory(new File("C:\\Users\\JC\\Documents"));
+	    
 	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
 	            "JPG & GIF Images", "jpg", "gif", "png", "txt");
 	    chooser.setFileFilter(filter);
@@ -16,15 +21,16 @@ public class retrieveFile {
 	    try {
 	        if(returnVal == JFileChooser.APPROVE_OPTION) {
 	            System.out.println("You chose to open this file: " +
-	                    chooser.getSelectedFile().getName());
+	                    chooser. getName());
 	        }	
 	    }
 	    catch (Exception e)
 	    {
 	    	System.out.println(e);
-	    }
-		
+	    }		
 	}
+	
+	//Method: https://www.youtube.com/watch?v=A6sA9KItwpY&t=11s
 
 	
 	public static void main(String[] args) {
