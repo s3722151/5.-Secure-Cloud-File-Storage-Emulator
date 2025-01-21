00aaeb5d337c6592ee5dd205d7007837cc7e8639
Zetazone <37312796+Zetazone@users.noreply.github.com>
2025-01-21 12:17:38 +1100

    21/1/2025 I am having having the following issue: 	"Error: LinkageError occurred while loading main class decryptFile 	java.lang.UnsupportedClassVersionError: decryptFile (class file version 66.65535) was compiled with preview features that are unsupported. 	This version of the Java Runtime only recognizes preview features for class file version 67.6553" https://www.oracle.com/au/java/technologies/downloads/#java21 I have tried re-installing and changing the jdk. I think I need to to try using an older version, 23 to 21.

--- src/oldCode.java
+++ src/oldCode.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
+//
 public class oldCode {
 	
 	//Method 1: https://stackoverflow.com/questions/40255039/how-to-choose-file-in-java
